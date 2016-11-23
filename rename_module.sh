df3594bc51f0a43c6ff17c41d7d6ab4b6de2dfb3
kukdh1 <kukdh1@gmail.com>
2016-11-23 22:32:15 +0900

    fix typo

--- rename_module.sh
+++ rename_module.sh
@@ -4,7 +4,7 @@ install_name_tool -change /usr/local/Cellar/ffmpeg/3.2/lib/libavutil.55.dylib @e
 
 install_name_tool -change /usr/local/Cellar/ffmpeg/3.2/lib/libswresample.2.dylib @executable_path/../Frameworks/libswresample.2.dylib ./Frameworks/libavformat.57.dylib
 install_name_tool -change /usr/local/Cellar/ffmpeg/3.2/lib/libavutil.55.dylib @executable_path/../Frameworks/libavutil.55.dylib ./Frameworks/libavformat.57.dylib
-install_name_tool -change /usr/local/Cellar/ffmpeg/3.2/lib/libavcodec.57.dylib @exeuctable_path/../Frameworks/libavcodec.57.dylib ./Frameworks/libavformat.57.dylib
+install_name_tool -change /usr/local/Cellar/ffmpeg/3.2/lib/libavcodec.57.dylib @executable_path/../Frameworks/libavcodec.57.dylib ./Frameworks/libavformat.57.dylib
 
 install_name_tool -change /usr/local/Cellar/ffmpeg/3.2/lib/libavutil.55.dylib @executable_path/../Frameworks/libavutil.55.dylib ./Frameworks/libswresample.2.dylib
 
